5bc43ba00c6d09fc5079257ac7d700fc783815d0
Ben Yanke <ben@benyanke.com>
2018-06-27 23:24:33 -0500

    Running yapf formatter

--- githelp.py
+++ githelp.py
@@ -4,57 +4,59 @@ import sys, os
 
 
 class GitHelp:
-   'Git CLI helper for developers'
-   cliOptions = {
-     'actions': [
-       {
-         'name':'newfeature',
-         'description':'Create a new feature branch, with some safety checks',
-         'handlerFunction':'featureBranchHandler',
-         'flags':[
-           {
-             'shortForm': 't',
-             'longForm': 'ticket',
-             'description': 'Ticket number',
-             'isRequired': True
-           },
-           {
-             'shortForm': 'f',
-             'longForm': 'feature',
-             'description': 'Short feature description',
-             'isRequired': True
-           },
-           {
-             'shortForm': 'v',
-             'longForm': 'verbose',
-             'description': 'Make output verbose',
-             'isRequired': False
-           },
-         ]
-       },
-     ],
+    'Git CLI helper for developers'
+    cliOptions = {
+        'actions': [
+            {
+                'name':
+                'newfeature',
+                'description':
+                'Create a new feature branch, with some safety checks',
+                'handlerFunction':
+                'featureBranchHandler',
+                'flags': [
+                    {
+                        'shortForm': 't',
+                        'longForm': 'ticket',
+                        'description': 'Ticket number',
+                        'isRequired': True
+                    },
+                    {
+                        'shortForm': 'f',
+                        'longForm': 'feature',
+                        'description': 'Short feature description',
+                        'isRequired': True
+                    },
+                    {
+                        'shortForm': 'v',
+                        'longForm': 'verbose',
+                        'description': 'Make output verbose',
+                        'isRequired': False
+                    },
+                ]
+            },
+        ],
+    }
 
-   }
+    def __init__(self):
+        print("startup")
 
-   def __init__(self):
-     print("startup")
+    def cliHandler(self, args):
+        print(args)
+        print(self.cliOptions)
 
-   def cliHandler(self, args):
-     print(args)
-     print(self.cliOptions)
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
 
-   def displayCount(self):
-     print ("Total Employee %d" % Employee.empCount)
+    def displayEmployee(self):
+        print("Name : ", self.name, ", Salary: ", self.salary)
 
-   def displayEmployee(self):
-      print ("Name : ", self.name,  ", Salary: ", self.salary)
+    def featureBranchHandler(self, options):
+        print("Invoking handler function")
+        print(options)
 
-   def featureBranchHandler(self,options):
-      print("Invoking handler function")
-      print(options)
 
 # print(os.popen("git status").read())
-
 
 if __name__ == '__main__':
 
@@ -66,3 +68,4 @@ if __name__ == '__main__':
 
     # Pass to the handler
     githelp.cliHandler(args)
+
