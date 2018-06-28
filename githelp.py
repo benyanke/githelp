834e961cb570c5d288a9faf8e71cb589dcb0e981
Ben Yanke <ben@benyanke.com>
2018-06-27 23:08:12 -0500

    Snapshot commit - starting to implement the TODO items, setting up the CLI handler framework

--- githelp.py
+++ githelp.py
@@ -2,10 +2,67 @@
 
 import sys, os
 
-args = sys.argv
 
-for arg in args:
-    print("  " + arg)
+class GitHelp:
+   'Git CLI helper for developers'
+   cliOptions = {
+     'actions': [
+       {
+         'name':'newfeature',
+         'description':'Create a new feature branch, with some safety checks',
+         'handlerFunction':'featureBranchHandler',
+         'flags':[
+           {
+             'shortForm': 't',
+             'longForm': 'ticket',
+             'description': 'Ticket number',
+             'isRequired': True
+           },
+           {
+             'shortForm': 'f',
+             'longForm': 'feature',
+             'description': 'Short feature description',
+             'isRequired': True
+           },
+           {
+             'shortForm': 'v',
+             'longForm': 'verbose',
+             'description': 'Make output verbose',
+             'isRequired': False
+           },
+         ]
+       },
+     ],
 
-print(os.popen("git status").read())
+   }
 
+   def __init__(self):
+     print("startup")
+
+   def cliHandler(self, args):
+     print(args)
+     print(self.cliOptions)
+
+   def displayCount(self):
+     print ("Total Employee %d" % Employee.empCount)
+
+   def displayEmployee(self):
+      print ("Name : ", self.name,  ", Salary: ", self.salary)
+
+   def featureBranchHandler(self,options):
+      print("Invoking handler function")
+      print(options)
+
+# print(os.popen("git status").read())
+
+
+if __name__ == '__main__':
+
+    githelp = GitHelp()
+
+    # Get args
+    args = sys.argv
+    del args[0]
+
+    # Pass to the handler
+    githelp.cliHandler(args)
